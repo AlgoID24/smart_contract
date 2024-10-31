ce97ca516eb9f7cbdefe6105069e79e3896deacf
Ezekiel Victor <codeepoch@gmail.com>
2024-10-31 19:23:16 +0100

    initial commit

--- projects/contracts/smart_contracts/authentication/contract.py
+++ projects/contracts/smart_contracts/authentication/contract.py
@@ -1,8 +1,22 @@
-from algopy import ARC4Contract, String
-from algopy.arc4 import abimethod
-
+from algopy import ARC4Contract, Txn, arc4, urange
 
 class Authentication(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def __init__(self) -> None:
+        self.admins: arc4.DynamicArray[arc4.Address] = arc4.DynamicArray[arc4.Address]()
+        self.admins.append(arc4.Address.from_bytes(Txn.sender.bytes))
+
+
+    @arc4.abimethod(allow_actions=["UpdateApplication"])
+    def update(self) -> bool:
+        for index in urange(self.admins.length):
+            value = self.admins[index]
+            return value.native == Txn.sender
+        return False
+
+
+    @arc4.abimethod(allow_actions=["DeleteApplication"])
+    def delete(self) -> bool:
+        for index in urange(self.admins.length):
+            value = self.admins[index]
+            return value.native == Txn.sender
+        return False
--- projects/contracts/tests/authentication_client_test.py
+++ projects/contracts/tests/authentication_client_test.py
@@ -5,7 +5,7 @@ from algokit_utils.config import config
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
 
-from smart_contracts.artifacts.authentication.authentication_client import AuthenticationClient
+from smart_contracts.artifacts.authentication.authentication_client import AuthenticationClient, DeleteArgs, Deploy, UpdateArgs
 
 
 @pytest.fixture(scope="session")
@@ -24,25 +24,27 @@ def authentication_client(
     )
 
     client.deploy(
+        update_args=Deploy[UpdateArgs](args=UpdateArgs()),
+        delete_args=Deploy[DeleteArgs](args=DeleteArgs()),
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
         on_update=algokit_utils.OnUpdate.AppendApp,
     )
     return client
 
 
-def test_says_hello(authentication_client: AuthenticationClient) -> None:
-    result = authentication_client.hello(name="World")
+def update(authentication_client: AuthenticationClient) -> None:
+    result = authentication_client.update_update()
 
     assert result.return_value == "Hello, World"
-
-
-def test_simulate_says_hello_with_correct_budget_consumed(
-    authentication_client: AuthenticationClient, algod_client: AlgodClient
-) -> None:
-    result = (
-        authentication_client.compose().hello(name="World").hello(name="Jane").simulate()
-    )
-
-    assert result.abi_results[0].return_value == "Hello, World"
-    assert result.abi_results[1].return_value == "Hello, Jane"
-    assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
+#
+#
+# def test_simulate_says_hello_with_correct_budget_consumed(
+#     authentication_client: AuthenticationClient, algod_client: AlgodClient
+# ) -> None:
+#     result = (
+#         authentication_client.compose().hello(name="World").hello(name="Jane").simulate()
+#     )
+#
+#     assert result.abi_results[0].return_value == "Hello, World"
+#     assert result.abi_results[1].return_value == "Hello, Jane"
+#     assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
