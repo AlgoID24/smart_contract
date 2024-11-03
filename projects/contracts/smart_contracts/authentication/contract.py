ccc0aa4f33da4ebb6a8dfe446e39c44d048b0c61
Kodesk <kodesk02@gmail.com>
2024-11-03 22:31:41 +0100

    Add: smart contract

--- projects/contracts/smart_contracts/authentication/contract.py
+++ projects/contracts/smart_contracts/authentication/contract.py
@@ -1,4 +1,5 @@
-from algopy import ARC4Contract, Txn, arc4, urange
+from algopy import ARC4Contract, Txn, arc4, urange, itxn, Global
+from algopy.op import ITxn
 
 class Authentication(ARC4Contract):
     def __init__(self) -> None:
@@ -20,3 +21,32 @@ class Authentication(ARC4Contract):
             value = self.admins[index]
             return value.native == Txn.sender
         return False
+    
+
+    @arc4.abimethod()
+    def create_algo_id(self, user_address: arc4.Address, full_name:arc4.String,  metadata_url:arc4.String, unit_name: arc4.String) -> arc4.UInt64:
+        algo_id_txn = itxn.AssetConfig(
+            fee= 1000,
+            total= 1,
+            unit_name= unit_name.native,
+            asset_name= full_name.native,
+            url=  metadata_url.native,
+            manager= Global.current_application_address,
+            reserve= Global.current_application_address,
+            freeze= Global.current_application_address,
+            clawback= Global.current_application_address,
+        )
+        algo_id_txn.submit()
+        asset = ITxn.created_asset_id()
+
+        asset_freeze_txn = itxn.AssetFreeze(
+            fee= 1000,
+            freeze_asset= asset,
+            freeze_account= user_address.native,
+            frozen= True,
+        )
+
+        asset_freeze_txn.submit()
+        return arc4.UInt64(asset.id)
+
+
--- projects/contracts/tests/authentication_client_test.py
+++ projects/contracts/tests/authentication_client_test.py
@@ -1,16 +1,24 @@
+import os
 import algokit_utils
 import pytest
-from algokit_utils import get_localnet_default_account
+from algosdk import mnemonic
+from algokit_utils import Account
 from algokit_utils.config import config
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
 
 from smart_contracts.artifacts.authentication.authentication_client import AuthenticationClient, DeleteArgs, Deploy, UpdateArgs
 
+@pytest.fixture(scope="session")
+def live_account() -> Account:
+    mnemonic_phrase = os.getenv("KODES_ENVIRONMENT")
+    assert mnemonic_phrase, "KODES_ENVIRONMENT: Error"
+    return Account(private_key = mnemonic.to_private_key(mnemonic_phrase))
+
 
 @pytest.fixture(scope="session")
 def authentication_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient
+    algod_client: AlgodClient, live_account: Account,
 ) -> AuthenticationClient:
     config.configure(
         debug=True,
@@ -19,20 +27,21 @@ def authentication_client(
 
     client = AuthenticationClient(
         algod_client,
-        creator=get_localnet_default_account(algod_client),
-        indexer_client=indexer_client,
+        app_id = 728344999,
+        signer= live_account,
+        sender= live_account.address,
     )
 
-    client.deploy(
-        update_args=Deploy[UpdateArgs](args=UpdateArgs()),
-        delete_args=Deploy[DeleteArgs](args=DeleteArgs()),
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
-    )
+    # client.deploy(
+    #     update_args=Deploy[UpdateArgs](args=UpdateArgs()),
+    #     delete_args=Deploy[DeleteArgs](args=DeleteArgs()),
+    #     on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
+    #     on_update=algokit_utils.OnUpdate.AppendApp,
+    # )
     return client
 
 
-def update(authentication_client: AuthenticationClient) -> None:
+def test_update(authentication_client: AuthenticationClient) -> None:
     result = authentication_client.update_update()
 
     assert result.return_value == "Hello, World"
